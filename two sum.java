5cf4b1c2a4e4afc953a2213958d8cac257f5c271
WestBridge <li1779716319@163.com>
2016-10-08 14:41:18 +0800

    Update two sum.java

--- two sum.java
+++ two sum.java
@@ -1,6 +1,8 @@
 /*
+         * 题目：在int型数组nums中找两个数之和为target，返回这两个数的下标。
+	 *
 	 * 思路：遍历一遍nums中所有的数，计算complement=target-nums[i]，如果complement在hashmap中，
-	 * 返回complement的下标和i,否则就把这个数存储，值为key,对应的下标为value。
+	 * 返回complement的下标和i,否则就把nums[i]存储到hashmap中，值为key,对应的下标为value。
 	 * 
 	 */
    public static int[] twoSum(int[] nums, int target) {
